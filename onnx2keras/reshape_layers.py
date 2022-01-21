0c135842a1bcb0bdf1fb57ece0fee14605ae2d2e
dawnclaude <60581017+dawnclaude@users.noreply.github.com>
2022-01-21 15:35:27 +0300

    Update reshape_layers.py

--- onnx2keras/reshape_layers.py
+++ onnx2keras/reshape_layers.py
@@ -374,10 +374,11 @@ def convert_squeeze(node, params, layers, lambda_func, node_name, keras_name):
         assert AttributeError('More than 1 input for squeeze layer.')
 
     input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
-    print(params)
-    def target_layer(x, axis=params['dim']):
+    if params['axes'][0] != 0:
+        print('Squeeze all shape 1 dims')
+    def target_layer(x):
         from tensorflow import keras
-        return keras.backend.squeeze(x, axis)
+        return keras.backend.squeeze(x)
 
     lambda_layer = keras.layers.Lambda(target_layer, name=keras_name)
     layers[node_name] = lambda_layer(input_0)
