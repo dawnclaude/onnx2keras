78654a53d2e0a615bf63a150c28672b46a33e61e
dawnclaude <60581017+dawnclaude@users.noreply.github.com>
2022-01-21 15:25:17 +0300

    Update reshape_layers.py

--- onnx2keras/reshape_layers.py
+++ onnx2keras/reshape_layers.py
@@ -375,7 +375,7 @@ def convert_squeeze(node, params, layers, lambda_func, node_name, keras_name):
 
     input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
 
-    def target_layer(x, axis=params['axes'][0]):
+    def target_layer(x, axis=params['dim']):
         from tensorflow import keras
         return keras.backend.squeeze(x, axis)
 
